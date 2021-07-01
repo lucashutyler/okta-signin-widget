672081ab1785f0473e97aa4f4ac105f58a4bc0ac
Jasleen Nuno <jasleen.nuno@okta.com>
2021-07-01 10:57:30 -0700

    change Fastpass to FastPass

--- test/testcafe/spec/IdentifyOktaVerify_spec.js
+++ test/testcafe/spec/IdentifyOktaVerify_spec.js
@@ -53,9 +53,9 @@ test('should the correct title', async t => {
 test('should the correct content', async t => {
   const identityPage = await setup(t);
   await t.expect(identityPage.getPageTitle()).eql('Sign In');
-  await t.expect(identityPage.getOktaVerifyButtonText()).eql('Sign in with Okta Fastpass');
+  await t.expect(identityPage.getOktaVerifyButtonText()).eql('Sign in with Okta FastPass');
   await t.expect(identityPage.getSeparationLineText()).eql('or');
   await identityPage.clickOktaVerifyButton();
   const header = new Selector('h2[data-se="o-form-head"]');
-  await t.expect(header.textContent).eql('Sign in with Okta Fastpass');
+  await t.expect(header.textContent).eql('Sign in with Okta FastPass');
 });
