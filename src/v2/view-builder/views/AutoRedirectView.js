f3932eef82c01a7522299c7a51e731a1755e2eb0
Jasleen Nuno <jasleen.nuno@okta.com>
2021-07-01 14:09:40 -0700

    will not update the AutoRedirectView

--- src/v2/view-builder/views/AutoRedirectView.js
+++ src/v2/view-builder/views/AutoRedirectView.js
@@ -1,11 +1,30 @@
-import { loc } from 'okta';
+import { _, loc } from 'okta';
 import { BaseForm, BaseView } from '../internals';
 
 const Body = BaseForm.extend({
   title() {
-    return loc('oie.success.text.signingIn.with.fastpass', 'login');
-  },
+    let titleString = loc('oie.success.text.signingIn', 'login');
+    // For more info on the API response available in appState, see IdxResponseBuilder.java
+    const app = this.options.appState.get('app');
+    const user = this.options.appState.get('user');
 
+    if (_.isEmpty(app)) {
+      return titleString;
+    }
+
+    const { label: appInstanceName, name: appDisplayName } = app;
+    const { identifier: userEmail } = user || {};
+
+    const appName = appInstanceName ? appInstanceName : appDisplayName;
+
+    if (appName && userEmail) {
+      titleString = loc('oie.success.text.signingIn.with.appName.and.identifier', 'login', [appName, userEmail]);
+    } else if (appName) {
+      titleString = loc('oie.success.text.signingIn.with.appName', 'login', [appName]);
+    }
+
+    return titleString;
+  },
   noButtonBar: true,
   initialize() {
     BaseForm.prototype.initialize.apply(this, arguments);
