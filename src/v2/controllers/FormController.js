0aeb030286e02ee4be4e49a2bdf66cc1e2385248
denys.oblohin <denys.oblohin@okta.om>
2021-07-04 20:56:01 +0300

    new test should fail w/o restart flow

--- src/v2/controllers/FormController.js
+++ src/v2/controllers/FormController.js
@@ -158,7 +158,7 @@ export default Controller.extend({
           this.showFormErrors(this.formView.model, error);
         })
         .then(() => {
-          if (actionPath === 'cancel') {
+          if (actionPath === 'cancel_____') {
             // Restart login flow
             this.options.settings.restoreOieSettings();
             startLoginFlow(this.options.settings)
